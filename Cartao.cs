066accb18a77bf529ab32f70f24dbd20b5245297
Beatriz <biasilva.lima118@gmail.com>
2020-06-10 09:05:06 -0300

    ajustes

--- Cartao.cs
+++ Cartao.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Aula13Uber
 {
     public class Cartao
@@ -7,12 +9,18 @@ namespace Aula13Uber
         public string Bandeira { get; set; }
         public string cvv { get; set; }
 
-        public string Cadastrar(){
+        public string Cadastrar(string titular)
+        {
             return "Cartão cadastrado";
         }
 
         public string Excluir(){
             return "Cartão cancelado e excluído";
+        }
+
+        internal void Cadastrar(string titular, string cvv)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,36 @@ namespace Aula13Uber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Usuario user = new Usuario();
+            System.Console.WriteLine("Digite seu login:  ");
+            string login = Console.ReadLine();
+
+            System.Console.WriteLine("Digite sua senha:  ");
+            string senha = Console.ReadLine();
+
+            user.Login(login, senha);
+
+                if(user.TokenLogin != "" && user.TokenLogin != null){  
+                System.Console.WriteLine("Acesso liberado, Usuário logado");
+                System.Console.WriteLine(user.TokenLogin);
+            }else{
+                System.Console.WriteLine("Usuário deslogado");
+            }
+
+            Cartao mastercard = new Cartao();
+
+            mastercard.Titular = "Beatriz";
+            mastercard.cvv = "107";
+            
+        System.Console.WriteLine("Digite nome do titular do cartão:  ");
+        string Titular = Console.ReadLine();
+
+        System.Console.WriteLine("Digite o cvv do cartão: ");
+        string cvv = Console.ReadLine();
+
+        mastercard.Cadastrar(Titular, cvv);
+        System.Console.WriteLine("Cartão Cadastrado" +mastercard.Titular, mastercard.cvv);
+        
         }
     }
 }
